e936edbd07672b3668a0312c441c2715180a7129
corndoghh <corndoghh@gmail.com>
2022-01-21 23:13:41 +0100

    Calculated for scroll offset

--- public/scripts/Home.js
+++ public/scripts/Home.js
@@ -1,9 +1,9 @@
 function getCoords() {
     var element = document.getElementById('TheMV');
     var position = element.getBoundingClientRect();
-    var x = position.left;
-    var y = position.bottom;
+    var y = position.bottom + window.scrollY;
     document.getElementById('infomation').style.top = y + 50 + 'px';
+    console.log(y);
 }
 
 var resizeObserver = new ResizeObserver(function (entries) {
