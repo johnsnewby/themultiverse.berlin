8c40bee66e016196caa139b15f20ecbf58128155
corndoghh <corndoghh@gmail.com>
2022-01-21 20:44:54 +0100

    Website update

---
+++ node/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static(path.join(__dirname + '../public/css')));
+app.use('/js', express.static(path.join(__dirname + '../public/scripts')));
+app.use('/img', express.static(path.join(__dirname + '../public/img')));
+
+
+app.set('views', path.join(__dirname, '../public/pages'));
+app.set('view engine', 'ejs');
+
+
+app.listen(
+    80,
+    () => console.log('Server started on port 80')
+)
+
+app.get(`/`, (req, res) => {
+    res.render('Home');
+});
