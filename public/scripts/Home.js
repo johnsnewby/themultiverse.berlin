bca545fbf859e15a7facfbe26e937ac89670d8c2
corndoghh <corndoghh@gmail.com>
2022-01-21 22:53:04 +0100

    Responsive image change and added js script that sets the text to the correct height depending on the height of the image

---
+++ public/scripts/Home.js
@@ -0,0 +1,14 @@
+function getCoords() {
+    var element = document.getElementById('TheMV');
+    var position = element.getBoundingClientRect();
+    var x = position.left;
+    var y = position.bottom;
+    document.getElementById('infomation').style.top = y + 50 + 'px';
+}
+
+var resizeObserver = new ResizeObserver(function (entries) {
+    entries.forEach(function (entry) {
+        getCoords();
+    });
+});
+resizeObserver.observe(document.getElementById('TheMV'));
