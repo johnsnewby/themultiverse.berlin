f2e6da2f55cf4771c6ad47699755844e34028d3d
John Newby <john@newby.org>
2022-01-24 15:45:45 +0100

    Small changes

--- node/app.js
+++ node/app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const i18n = require('i18n-express');
 const path = require('path');
+var favicon = require('serve-favicon');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
+
 app.use(i18n({
   defaultLang: 'de',
   siteLangs: ['en', 'de', 'se'],
   textsVarName: 't',
   translationsPath: path.join(__dirname, '../locales')
 }));
+
 app.use('/css', express.static(path.join(__dirname + '../public/css')));
 app.use('/js', express.static(path.join(__dirname + '../public/scripts')));
 app.use('/img', express.static(path.join(__dirname + '../public/img')));
+app.use(favicon(path.join(__dirname,'../public','img','favicon.ico')));
 
 
 app.set('views', path.join(__dirname, '../public/pages'));
