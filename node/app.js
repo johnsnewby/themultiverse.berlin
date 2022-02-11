d866fb2ccd3b6852915ced8a8590b4d5c760517f
John Newby <john@newby.org>
2022-02-11 14:33:57 +0100

    GDPR -- fonts locally: :

--- node/app.js
+++ node/app.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
 app.use(express.static(path.join(__dirname, '../public')));
 
